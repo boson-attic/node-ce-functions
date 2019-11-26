6cb2972993b5640aa3fcc56ef71795b6a46a4f82
Lance Ball <lball@redhat.com>
2019-11-26 17:48:33 -0500

    Initial commit

---
+++ image/project/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const path = require('path');
+const framework = require('@redhat/faas-js-runtime');
+
+const userPath = path.join(`${__dirname}`, 'usr');
+const LISTEN_PORT = process.env.LISTEN_PORT || 8080;
+
+let server;
+
+module.exports.close = function close() {
+  if (server) server.close();
+}
+
+try {
+  framework(require(userPath), LISTEN_PORT, srv => {
+    console.log('FaaS framework initialized');
+    server = srv;
+  });
+} catch (err) {
+  console.error(`Cannot load user function at ${userPath}`);
+  throw err;
+}
---
+++ image/project/test/test.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const runtime = require('..');
+const test = require('tape');
+const request = require('supertest');
+
+// test('Loads a user function with dependencies', t => {
+//   framework(func, server => {
+//     t.plan(2);
+//     request(server)
+//       .get('/')
+//       .expect(200)
+//       .expect('Content-Type', /json/)
+//       .end((err, res) => {
+//         t.error(err, 'No error');
+//         t.equal(res.body, 'This is the test function for Node.js FaaS. Success.');
+//         t.end();
+//         server.close();
+//       });
+//     }, { log: false });
+// });
+
+
+// test('Can respond via an async function', t => {
+//   framework(func, server => {
+//     t.plan(2);
+//     request(server)
+//       .get('/')
+//       .expect(200)
+//       .expect('Content-Type', /json/)
+//       .end((err, res) => {
+//         t.error(err, 'No error');
+//         t.equal(res.body,
+//           'This is the test function for Node.js FaaS. Success.');
+//         t.end();
+//         server.close();
+//       });
+//     }, { log: false });
+// });
+const server = 'http://localhost:8080';
+
+test('Exposes readiness URL', t => {
+  t.plan(2);
+  request(server)
+    .get('/health/readiness')
+    .expect(200)
+    .expect('Content-type', /text/)
+    .end((err, res) => {
+      t.error(err, 'No error');
+      t.equal(res.text, 'OK');
+      t.end();
+    });
+});
+
+test('Exposes liveness URL', t => {
+  t.plan(2);
+  request(server)
+    .get('/health/liveness')
+    .expect(200)
+    .expect('Content-type', /text/)
+    .end((err, res) => {
+      t.error(err, 'No error');
+      t.equal(res.text, 'OK');
+      t.end();
+    });
+});
+
+test.onFinish(runtime.close);

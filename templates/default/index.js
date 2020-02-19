fe4291184f85a550c1e92f2042a136192805eedc
Lance Ball <lball@redhat.com>
2020-02-19 10:10:12 -0500

    template,test: make template function async and ensure it works

--- templates/default/index.js
+++ templates/default/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-module.exports = function (context) {
+module.exports = async function (context) {
   if (!context.cloudevent) {
-    return new Error('No cloud event received');
+    return Promise.reject(new Error('No cloud event received'));
   }
-  context.log.info(`Cloud event received: ${context.cloudevent}`);
-  return {
-    data: context.cloudevent.data
-  }
+  context.log.info(`Cloud event received: ${JSON.stringify(context.cloudevent)}`);
+  return new Promise((resolve, reject) => {
+    setTimeout(_ => resolve({ data: context.cloudevent.data }), 500);
+  });
 };
